27dced69b4b76efe6f1c68720f7a8bd3a415a589
Elisee Augustin <eliseeaugustin@elisees-mbp-2.mynetworksettings.com>
2025-03-13 20:01:47 -0400

    Added functionality for session data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import { getServerSession } from 'next-auth'
 import { prisma } from '../lib/prisma'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { User } from './user'
 
 const Home = async () => {
   const user = await prisma.user.findFirst({
     where: { email: 'test@test.com' },
   })
-  return <div>Hello {user?.name}</div>
+
+  const session = await getServerSession(authOptions) //can use this for getting session data after login
+  return (
+    <div>
+      Hello {user?.name}
+      {JSON.stringify(session)}
+      <User />
+    </div>
+  )
 }
 
 export default Home
